74fb7e602ce52b8210e4736f786282e90d8fbe66
Marina Medvedeva <medveider42@yandex.ru>
2023-04-05 15:28:40 +0300

    all elements

--- pages/index.js
+++ pages/index.js
@@ -1,10 +1,26 @@
 const openPopupBtn = document.querySelector('.profile__edit-button');
 const popup = document.querySelector('.popup');
+const popupCloseBtn = document.querySelector('.popup__close-bttn');
 
-function togglePopUp () {
-    popup.classList.toggle('.popup_opened');
+function togglePopUp() {
+    popup.classList.toggle('popup_opened');
 }
 
 openPopupBtn.addEventListener('click', togglePopUp);
+popupCloseBtn.addEventListener('click', togglePopUp);
 
-console.log('profile__edit-button');
+let formElement = document.querySelector('.popup__container');
+const nameInput = document.querySelector('.popup__field-name');
+const jobInput = document.querySelector('.popup__field-about');
+const Name = document.querySelector('.profile__name');
+const job = document.querySelector('.profile__about');
+nameInput.value = Name.textContent;
+jobInput.value = job.textContent;
+
+function handleFormSubmit(evt) {
+    evt.preventDefault();
+    Name.textContent = nameInput.value;
+    job.textContent = jobInput.value;
+}
+
+formElement.addEventListener('submit', handleFormSubmit);
